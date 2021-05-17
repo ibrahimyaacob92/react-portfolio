30b5e23443c99eb814d0e0aee487a57f11feed83
Ibrahim Yaacob <ibrahimyaacob92@yahoo.com>
2021-05-17 22:02:50 +0800

    add pdf resume section

---
+++ src/modules/Resume.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/Resume.css";
+
+const Resume = () => {
+  return (
+    <div className="grey-background">
+      <div className="resume">
+        <div className="side-pane">
+          <div>
+            <h1>Ibrahim Yaacob</h1>
+            <h2 className="italic">Full Stack Developer</h2>
+          </div>
+
+          <p>This is a paragraph</p>
+        </div>
+        <div>This</div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
